d910ad183887438835620bcf39e75b9c7b15df7f
Fábio Leandro Pereira Zambon <flzambon@pop-os.localdomain>
2021-07-20 17:49:12 -0300

    configurando conexao com oracle

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'oracle',
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      sid: process.env.TYPEORM_SID,
+      schema: process.env.TYPEORM_SCHEMA,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      entities: [],
+      synchronize: false,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
